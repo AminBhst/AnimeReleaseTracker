84254c02cd60065ea4b158051875bb1994109da8
AminBhst <amin.bhst@gmail.com>
2024-06-02 15:28:38 +0330

    Remove import

--- src/main/java/com/aminbhst/animereleasetracker/AnimeReleaseTrackerApplication.java
+++ src/main/java/com/aminbhst/animereleasetracker/AnimeReleaseTrackerApplication.java
@@ -1,12 +1,10 @@
 package com.aminbhst.animereleasetracker;
 
 import com.aminbhst.animereleasetracker.bot.AnimeReleaseTrackerBot;
-import com.aminbhst.animereleasetracker.core.job.SeasonalAnimeInitializerJob;
 import com.aminbhst.quartzautoconfigboot.annotation.EnableQuartzConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
